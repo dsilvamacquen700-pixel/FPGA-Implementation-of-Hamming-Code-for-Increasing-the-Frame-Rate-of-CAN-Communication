218bcf1af3ec6f560b9fc2d3f6d126f87eb6f630
joelpinto20 <joelpintomanipal@gmail.com>
2026-05-12 00:00:55 +0530

    Update testbench.v

--- tbcode/testbench.v
+++ tbcode/testbench.v
@@ -80,3 +80,86 @@ initial begin
     $finish;
 end
 endmodule
+
+
+
+
+module tb_shift_reg;
+reg clk;
+reg rst;
+reg din;
+wire [7:0] q;
+
+shift_reg uut (
+    .clk(clk),
+    .rst(rst),
+    .din(din),
+    .q(q)
+);
+always #5 clk = ~clk;
+initial begin
+    clk = 0;
+    rst = 0;
+    din = 0;
+    #1  rst = 1;
+    #10 rst = 0;
+    #5  din = 1;
+    #10 din = 0;
+    #10 din = 1;
+    #10 din = 1;
+    #10 din = 0;
+    #30 $finish;
+end
+endmodule
+
+
+
+module tb_parity;
+reg [3:0] d;
+wire p;
+
+parity_gen uut(d,p);
+
+initial begin
+    d=4'b0000; #10;
+    d=4'b1010; #10;
+    d=4'b1111; #10;
+    d=4'b0101; #10;
+    $finish;
+end
+endmodule
+
+
+
+
+module tb_syndrome;
+reg [6:0] r;
+wire [2:0] s;
+
+syndrome uut(r,s);
+
+initial begin
+    r=7'b0000000; #10;
+    r=7'b1010101; #10;
+    r=7'b1110001; #10;
+    r=7'b0101010; #10;
+    $finish;
+end
+endmodule
+
+
+
+module tb_fsm;
+reg clk=0, rst;
+wire state;
+
+simple_fsm uut(clk,rst,state);
+
+always #5 clk = ~clk;
+
+initial begin
+    rst=1; #10;
+    rst=0; #50;
+    $finish;
+end
+endmodule
